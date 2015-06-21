bcfa2df1389647b4010c4b9d97c4da5b58262df3
Devin Brown <devin.g.brown@gmail.com>
2015-06-21 12:24:31 -0600

    update customer route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'customers#index'
 
   get '/customers', to: 'customers#index'
-  get '/customer/:id', to: 'customers#show'
+  get '/customer/:id', to: 'customers#show', as: 'customer'
 end
